0bbfaa2490c5c91eddd9295e72a6a5076336e6db
Loganarmack <loganarmack@gmail.com>
2021-06-21 01:37:58 -0400

    Updated readme and made a few small fixes to game

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # SubstringGame
-A game in which the user has to enter a word containing a random substring.
+A word game in which the user is given a random substring and must enter
+a real word containing the substring.
+
+## To install packages:
+`$ pipenv install`
+
+You'll also need to download the list of words from nltk. To do this:
+```
+$ python
+>>> import nltk
+>>> nltk.download('words')
+```
+
+You can then run the game with 
+`pipenv run python game.py`
+
+
--- game.py
+++ game.py
@@ -1,12 +1,14 @@
 from word_counter import get_pair_levels, get_triplet_levels
-import enchant
+from nltk.corpus import words
 from random import randint, choice
 import constant
 from math import floor
 
+
 class Game():
-    eng_dict = enchant.Dict("en_US")
-    levels = {2: get_pair_levels(constant.LEVELS), 3: get_triplet_levels(constant.LEVELS)}
+    word_list = words.words()
+    levels = {2: get_pair_levels(constant.LEVELS),
+              3: get_triplet_levels(constant.LEVELS)}
 
     def __init__(self):
         self.__reset()
@@ -14,7 +16,8 @@ class Game():
     def choose_substr(self):
         self.substr_level = self.weighted_random(self.level_weights)
         self.substr_length = self.weighted_random(self.length_weights)
-        self.substr = choice(self.levels[self.substr_length][self.substr_level])
+        self.substr = choice(
+            self.levels[self.substr_length][self.substr_level])
 
     @staticmethod
     def weighted_random(weights):
@@ -24,48 +27,55 @@ class Game():
             r -= weights[level]
             if r <= 0:
                 return level
-            
+
     def start(self):
         self.__reset()
-        
+
         while(self.lives > 0):
             self.choose_substr()
 
-            user_word = input("Enter a word containing %s (level = %s):\n" % (self.substr, self.substr_level))
+            user_word = input("Enter a word containing %s (level = %s):\n" % (
+                self.substr, self.substr_level)).lower()
 
-            if user_word in self.used_words:
-                print("You've already used this word!")
-                self.lives -= 1
-            elif Game.eng_dict.check(user_word) and self.substr in user_word.lower():
-                self.score(user_word)
+            if (user_word
+                and user_word not in self.used_words
+                and user_word in Game.word_list
+                and self.substr in user_word
+            ):
+                self.score_word(user_word)
                 print("New score:", self.points)
                 self.used_words.append(user_word)
+                self.__increment_weights()
+
+            elif user_word in self.used_words:
+                print("You've already used this word!")
+                self.lives -= 1
+
             else:
                 print("Invalid word")
                 self.lives -= 1
-            
-            self.increment_weights()
 
         print("Game over. Your score is", self.points)
 
-    def score(self, word):
-        base = 100 # default for valid word
-        base += 10 * len(word) # bonus for making longer words
+    def score_word(self, word):
+        base = 100  # default for valid word
+        base += 10 * len(word)  # bonus for making longer words
         if word[:2] != self.substr and word[:3] != self.substr:
-            base += 50 # bonus for not starting word with substring
-        bonus_mult = self.substr_level / 4.0 # substr difficulty multiplier
-        bonus_mult += (self.substr_length - 2) / 2.0 # substr length multiplier
+            base += 50  # bonus for not starting word with substring
+        bonus_mult = self.substr_level / 4.0  # substr difficulty multiplier
+        bonus_mult += (self.substr_length - 2) / 2.0  # substr length multiplier
         self.points += base * (bonus_mult + 1)
 
-    def increment_weights(self):
+    def __increment_weights(self):
         for level in self.level_weights:
             if level != constant.LEVELS - 1:
-                self.level_weights[level + 1] += floor(self.level_weights[level] / 10.0)
-                self.level_weights[level] = floor(self.level_weights[level] * 9 / 10.0)
+                self.level_weights[level +
+                                   1] += floor(self.level_weights[level] / 10.0)
+                self.level_weights[level] = floor(
+                    self.level_weights[level] * 9 / 10.0)
 
         self.length_weights[3] += floor(self.length_weights[2] / 10.0)
         self.length_weights[2] = floor(self.length_weights[2] * 9 / 10.0)
-
 
     def __reset(self):
         self.points = 0
@@ -76,7 +86,7 @@ class Game():
         self.substr = "ng"
         self.substr_level = 0
         self.substr_length = 2
-        
-    
+
+
 game = Game()
 game.start()
